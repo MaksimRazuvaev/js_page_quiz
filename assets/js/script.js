7fae1d04fe3fb4c62411f5709e886c4c05ac1e7b
Maksim Razuvaev <maksim.razuvaev.wa@gmail.com>
2022-10-14 15:16:48 -0700

    pseudo code is written in js file

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+// Header
+// View Highscorse and Timer elements (div)
+// Timer and events: 
+// 1. Wrong answer slash
+// 2. End time to get to All done section
+
+// Questionary 5 questions (div):
+// 1. questions h1
+// 2. Answer options 4 (submit buttons)
+// 3. Wrong/Correct indicator (p)
+
+// All done section
+// Final score indicator (p)
+// Enter initials (form) + (submit button)
+
+// Highscorce section
+// Read only (form) with initials and score displayed
+// Go Back (button) to get back to All done section
+// Clear Higscore (button) to get to Codding Quiz Chalange
+// Without header
+
+// Highscorce section (Clear Highscore event)
+// Go Back (button) to get to Codding Quiz Chalange
+// Clear Higscore (button) to get to Codding Quiz Chalange
+// Without header
+
+// Coding Quiz Chalange
+// Intro paragraph (h3)
+// Start quiz (button)
+// Without header
